8e44c475e8a12b75fb95f70aaa00b6a0d9aafa94
JHenriqueN <jhnhenriquejhn@gmail.com>
2025-01-30 18:57:41 -0300

    Atualizando Lista de Amigos

--- challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,14 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Lista de Amigos
+let amigos = [];
+
+function atualizarListaAmigos() {
+    let listaContainer = document.getElementById('listaAmigos');
+    listaContainer.innerHTML = '';
+
+    amigos.forEach(amigo => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        listaContainer.appendChild(li);
+    });
+
+}
