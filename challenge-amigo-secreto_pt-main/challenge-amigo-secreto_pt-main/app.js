964314c433a17f6d5686feed5f5d6ab5884d6f52
JHenriqueN <jhnhenriquejhn@gmail.com>
2025-01-30 19:06:54 -0300

    Adicionando Amigos

--- challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
@@ -12,3 +12,18 @@ function atualizarListaAmigos() {
     });
 
 }
+// Adicionando Amigos
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').ariaValueMax.trim();
+    if (!nome) {
+        alert('Por favor, insira o nome dos amigos');
+
+    }else if (amigos.includes(nome)) {
+        alert('Esse amigo já está na lista!');
+
+    }else {
+        amigos.push(nome);
+        atualizarListaAmigos();
+    }
+    document.getElementById('amigo').value = '';
+}
